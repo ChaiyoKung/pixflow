341697df7a85718adff398ea51fe6f87fe33852b
ChaiyoKung <kananek45@gmail.com>
2025-02-23 21:56:45 +0700

    feat: add favicon and app icons, and create web manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,13 @@ import { TRPCReactProvider } from "~/trpc/react";
 export const metadata: Metadata = {
   title: "PixFlow",
   description: "PixFlow is an AI-powered application that automatically generates images on a schedule",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    { rel: "icon", sizes: "16x16", url: "/favicon-16x16.png" },
+    { rel: "icon", sizes: "32x32", url: "/favicon-32x32.png" },
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+  ],
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
