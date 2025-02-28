83f604c2c5f93d3ef863acda8bcef11dbe3d6b67
ChaiyoKung <kananek45@gmail.com>
2025-03-01 02:56:25 +0700

    feat: add custom error page

---
+++ src/app/error.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Button, Container, Stack, Title } from "@mantine/core";
+import { Header } from "./_components/header";
+import { Footer } from "./_components/footer";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <Stack mih="100vh">
+      <Header />
+
+      <Container
+        component="main"
+        flex={1}
+        display="flex"
+        style={{
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "var(--mantine-spacing-md)",
+        }}
+      >
+        <Title order={1}>Something went wrong!</Title>
+
+        <Link href="/" passHref legacyBehavior>
+          <Button component="a" radius="md">
+            Back to home page
+          </Button>
+        </Link>
+      </Container>
+
+      <Footer />
+    </Stack>
+  );
+}
