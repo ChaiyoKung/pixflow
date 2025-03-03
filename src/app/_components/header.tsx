7e1364ca4e5ce8635ffd845dfeaf7d3e84f1a7e8
ChaiyoKung <kananek45@gmail.com>
2025-03-04 00:09:19 +0700

    feat: enhance header styling with background color and adjusted blur effect

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
       p="xs"
       pos="sticky"
       top={0}
-      styles={{ root: { backdropFilter: "blur(5rem)", zIndex: 9999 } }}
+      styles={{ root: { backdropFilter: "blur(3rem)", zIndex: 9999, backgroundColor: "rgba(36, 36, 36, 0.75)" } }}
     >
       <Title order={1}>PixFlow</Title>
     </Flex>
