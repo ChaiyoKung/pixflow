cdf7e6af043986289c9111c62f408d0495c399ff
ChaiyoKung <kananek45@gmail.com>
2025-03-12 21:33:35 +0700

    feat: remove copyright year in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { Flex, Text } from "@mantine/core";
 export function Footer() {
   return (
     <Flex component="footer" justify="center" align="center" p="xs">
-      <Text>&copy; 2025 ChaiyoKung</Text>
+      <Text>&copy; ChaiyoKung</Text>
     </Flex>
   );
 }
